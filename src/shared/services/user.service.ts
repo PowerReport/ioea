f639793303d21fdaf6b14f6d767bcd954d19ae9d
drawmoon <1340260725@qq.com>
2021-04-16 23:29:38 +0800

    补充Injectable装饰器

--- src/shared/services/user.service.ts
+++ src/shared/services/user.service.ts
@@ -1,6 +1,8 @@
 import { IUserAccessor } from './user.accessor';
 import { UserDTO } from '../dto/user.dto';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class UserService implements IUserAccessor {
   current: UserDTO;
 }
