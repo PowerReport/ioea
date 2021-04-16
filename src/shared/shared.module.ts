0a7ffd5680aa0e1067e826eaab97ec93a29097a1
drawmoon <1340260725@qq.com>
2021-04-16 23:33:57 +0800

    :art: 修改变量命名

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { USER_ACCESSOR } from './services/user.accessor';
 import { UserService } from './services/user.service';
 
-const userAccessorProviders = {
+const userAccessorProvider = {
   provide: USER_ACCESSOR,
   useClass: UserService,
 };
 
 @Module({
-  providers: [userAccessorProviders],
-  exports: [userAccessorProviders],
+  providers: [userAccessorProvider],
+  exports: [userAccessorProvider],
 })
 export class SharedModule {}
