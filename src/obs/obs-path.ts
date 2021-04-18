fe254357fd41d9b4fad847c397ef818eb511892b
drawmoon <1340260725@qq.com>
2021-04-18 14:28:10 +0800

    提交部分 obs 服务方法的实现

---
+++ src/obs/obs-path.ts
@@ -0,0 +1,49 @@
+import * as path from 'path';
+
+export class ObsPath {
+  private readonly _src: string;
+  private readonly _type: ObsPathType;
+
+  constructor(src: string) {
+    if (!src) {
+      throw new Error('path cannot be null.');
+    }
+
+    this._type = ObsPath.isLocal(src) ? ObsPathType.Local : ObsPathType.Cloud;
+    this._src = ObsPath.toRealFilePath(this._src);
+  }
+
+  private static isLocal(src: string): boolean {
+    // 判断是否为绝对路径，如果是绝对路径则是本地路径，或者是否以 local: 开头
+    if (path.isAbsolute(src) || src.startsWith(ObsPathType.Local)) {
+      return true;
+    }
+
+    // 以 cloud: 开头为云端路径
+    if (src.startsWith(ObsPathType.Cloud)) {
+      return false;
+    }
+
+    throw new Error('not supported.');
+  }
+
+  private static toRealFilePath(src: string): string {
+    return src.replace(
+      ObsPath.isLocal(src) ? ObsPathType.Local : ObsPathType.Cloud,
+      '',
+    );
+  }
+
+  get realPath(): string {
+    return this._src;
+  }
+
+  get type(): ObsPathType {
+    return this._type;
+  }
+}
+
+export enum ObsPathType {
+  Cloud = 'cloud:',
+  Local = 'local:',
+}
--- src/obs/obs.service.ts
+++ src/obs/obs.service.ts
@@ -1,15 +1,63 @@
 import { IObsService } from './obs.interface';
+import { Injectable } from '@nestjs/common';
+import { Client } from 'minio';
+import { ObsPath, ObsPathType } from './obs-path';
 
+@Injectable()
 export class ObsService implements IObsService {
-  copy(source: string, target: string): Promise<void> {
+  readonly _minioClient: Client;
+  readonly _bucketName: string;
+
+  constructor() {
+    this._minioClient = new Client({
+      endPoint: '127.0.0.1',
+      port: 9000,
+      useSSL: true,
+      accessKey: 'Q3AM3UQ867SPQQA43P2F',
+      secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+    });
+    this._bucketName = 'ioea';
+  }
+
+  async copy(source: string, target: string): Promise<void> {
+    const src1 = new ObsPath(source);
+    const src2 = new ObsPath(target);
+
+    // 上传文件至云端
+    if (src1.type === ObsPathType.Local && src2.type === ObsPathType.Cloud) {
+      await this._minioClient.fPutObject(
+        this._bucketName,
+        src2.realPath,
+        src1.realPath,
+        {
+          'Content-Type': 'application/zip',
+        },
+      );
+      return;
+    }
+
+    // 下载文件至本地
+    if (src1.type === ObsPathType.Cloud && src2.type === ObsPathType.Local) {
+      await this._minioClient.fGetObject(
+        this._bucketName,
+        src1.realPath,
+        src2.realPath,
+      );
+      return;
+    }
+
     throw new Error('not implemented.');
   }
 
-  remove(target: string): Promise<void> {
-    throw new Error('not implemented.');
-  }
+  async remove(target: string): Promise<void> {
+    const src = new ObsPath(target);
 
-  private isLocal(src: string): boolean {
+    // 删除云端的文件
+    if (src.type === ObsPathType.Cloud) {
+      await this._minioClient.removeObject(this._bucketName, src.realPath);
+      return;
+    }
+
     throw new Error('not implemented.');
   }
 }
