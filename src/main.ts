4e2da9dd632184511965a7b488ccca11e3a59ad4
drawmoon <1340260725@qq.com>
2021-04-29 21:44:53 +0800

    :egg: 添加启动日志

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
+
+  logger.log(`
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\\  =  /0
+                    ___/\`---'\\___
+                  .' \\\\|     |// '.
+                 / \\\\|||  :  |||// \\
+                / _||||| -:- |||||- \\
+               |   | \\\\\\  - /// |   |
+               | \\_|  ''\\---/''  |_/ |
+               \\  .-\\__  '-'  ___/-. /
+             ___'. .'  /--.--\\  \`. .'___
+          ."" '<  \`.___\\_<|>_/___.' >' "".
+         | | :  \`- \`.;\`\\ _ /\`;.\`/ - \` : | |
+         \\  \\ \`_.   \\_ __\\ /__ _/   .-\` /  /
+     =====\`-.____\`.___ \_____/___.-\`___.-'=====
+                       \`=---='
+
+
+     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+         佛祖保佑       永不宕机     永无BUG
+`);
+
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -16,5 +45,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
+  logger.log('Now listening on: http://localhost:3000.');
 }
 bootstrap();
